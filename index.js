0512691b176470531dff360e1028cbbe0be408f0
Tuhin Islam <tuhin981172@gmail.com>
2022-05-17 17:12:58 +0600

    add

--- index.js
+++ index.js
@@ -161,6 +161,13 @@ async function run() {
             const doctor = req.body;
             const result = await doctorCollection.insertOne(doctor);
             res.send(result);
+        });
+
+        app.delete('/doctor/:email', verifyJWT, veryfyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await doctorCollection.deleteOne(filter);
+            res.send(result);
         })
 
     }
